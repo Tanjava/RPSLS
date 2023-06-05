30a2490db98ea8af1ea3f0c129e2d105df3a001f
Tanjava <tanja.pustjens@live.nl>
2023-06-05 23:44:20 +0000

    Add code to show player's choice and computer's choice (big emoji)

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.querySelectorAll('.btn');
     for (let button of buttons) {
         button.addEventListener('click', function () {
+
             let playerChoice = this.getAttribute('data-choice');
-            console.log('You chose ' + playerChoice);
+            console.log('Player chose ' + playerChoice);
+
             let houseChoice = randomHouse();
-            console.log('House chose ' + houseChoice);
+            console.log('Computer chose ' + houseChoice);
+
+            showHouseChoice(houseChoice);
+            showPlayerChoice(playerChoice);
         });
     };
 })
@@ -24,4 +29,20 @@ let gameOptions = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
 function randomHouse() {
     let random = Math.floor(Math.random() * gameOptions.length);
     return gameOptions[random];
+}
+
+function showHouseChoice(houseChoice) {
+    let houseSection = document.getElementById('house-section');
+    houseSection.innerHTML = `
+      <h2 class="score">You: <span id="player-score">0</span></h2>
+      <img src="assets/images/${houseChoice.toLowerCase()}.png" class="big-emoji-2" alt="${houseChoice} Emoji">
+    `;
+}
+
+function showPlayerChoice(playerChoice) {
+    let playerSection = document.getElementById('player-section');
+    playerSection.innerHTML = `
+      <h2 class="score">House: <span id="house-score">0</span></h2>
+      <img src="assets/images/${playerChoice.toLowerCase()}.png" class="big-emoji" alt="${playerChoice} Emoji">
+    `;
 }
