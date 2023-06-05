f999f2f1d3aa02e600d970f726b60ff80b5ce86c
Tanjava <tanja.pustjens@live.nl>
2023-06-05 22:22:10 +0000

    Add code to get a random choice from the computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
-// Wait for the DOM to finish loading before running the game
-// Add event listeners to the buttons
-
+/** 
+ * Wait for the DOM to finish loading before running the game
+ * Add event listeners to the buttons
+ */
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.querySelectorAll('.btn');
     for (let button of buttons) {
         button.addEventListener('click', function () {
             let playerChoice = this.getAttribute('data-choice');
-            console.log('You chose ' + playerChoice)
+            console.log('You chose ' + playerChoice);
+            let houseChoice = randomHouse();
+            console.log('House chose ' + houseChoice);
         });
     };
 })
 
 
 let gameOptions = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+
+
+/**
+ * Get the computer's choice (random)
+ */
+function randomHouse() {
+    let random = Math.floor(Math.random() * gameOptions.length);
+    return gameOptions[random];
+}
